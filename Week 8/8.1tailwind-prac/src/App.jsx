b7144254fa886ce66bdef4a8a1991f06494d083e
Rishu <kumarrishu571@gmail.com>
2024-02-06 11:23:51 +0530

    week 8.1

---
+++ Week 8/8.1tailwind-prac/src/App.jsx
@@ -0,0 +1,59 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+
+      {/* <div style={{
+        display: "flex",
+        justifyContent: "space-between"
+      }}>
+        <div style={{backgroundColor: "red"}}>
+          This is first div
+        </div>
+        <div style={{backgroundColor: "blue"}}>
+          This is second div 
+        </div>
+        <div style={{backgroundColor: "green"}}>
+          This is third div 
+        </div>
+      </div>
+
+      <div className='flex justify-between'>
+          <div className='bg-red-500'>
+              This is first div
+          </div>
+          <div className='bg-yellow-500'>
+              This is second div 
+          </div>
+          <div className='bg-green-500'>
+              This is third div 
+          </div>
+      </div>
+
+      <div className='grid grid-cols-10'>
+        <div className='bg-red-400 col-span-4'>Hii there form div 1</div>
+        <div className='bg-blue-400 col-span-4'>Hii there from div 2</div>
+        <div className='bg-yellow-400 col-span-2'>Hii there from div 3</div>
+      </div>
+
+      <div className='flex'>
+      <div className='bg-red-400 w-2/5'>Hii there form div 1</div>
+        <div className='bg-blue-400 w-2/5'>Hii there from div 2</div>
+        <div className='bg-yellow-400 w-1/5'>Hii there from div 3</div>
+      </div>
+
+      <div className='bg-red-500 md:bg-blue-500'>Hii from rishu</div> */}
+
+      <div className='grid grid-cols-1 md:grid-cols-3'>
+        <div className='bg-red-400'>1</div>
+        <div className='bg-yellow-400'>2</div>
+        <div className='bg-purple-400'>3</div>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
